eab0449dad3032648b3c3ba33eb2ea768814b189
Ralfe Poisson <ralfe@implyit.co.za>
2013-09-30 10:33:09 +0200

    Started adding code for incoming socket connections.

--- include/models/platform.class.php
+++ include/models/platform.class.php
@@ -17,11 +17,18 @@ class Platform {
 	
 	public $apps;
 	public $context;
+	public $address;
+	public $port;
+	public $socket;
 	
 	public function __construct() {
 		# Set Attributes
 		$this->apps														= array();
 		$this->context													= 0;
+		
+		# Set Congiruation Properties
+		set_time_limit(0);
+		ob_implicit_flush();
 	}
 	
 	/*	-------------------------------------------------------
@@ -53,7 +60,44 @@ class Platform {
 	}
 	
 	public function run() {
+		# Create Socket Connection
+		$this->init_socket();
 		
+		# Loop
+		do {
+		    if (($msgsock = socket_accept($this->socket)) === false) {
+		        echo "socket_accept() failed: reason: " . socket_strerror(socket_last_error($this->socket)) . "\n";
+		        break;
+		    }
+		    /* Send instructions. */
+		    $msg = "\nWelcome to the PHP Test Server. \n" .
+		        "To quit, type 'quit'. To shut down the server type 'shutdown'.\n";
+		    socket_write($msgsock, $msg, strlen($msg));
+
+		    do {
+		        if (false === ($buf = socket_read($msgsock, 2048, PHP_NORMAL_READ))) {
+		            echo "socket_read() failed: reason: " . socket_strerror(socket_last_error($msgsock)) . "\n";
+		            break 2;
+		        }
+		        if (!$buf = trim($buf)) {
+		            continue;
+		        }
+		        if ($buf == 'quit') {
+		            break;
+		        }
+		        if ($buf == 'shutdown') {
+		            socket_close($msgsock);
+		            break 2;
+		        }
+		        $talkback = "PHP: You said '$buf'.\n";
+		        socket_write($msgsock, $talkback, strlen($talkback));
+		        echo "$buf\n";
+		    } while (true);
+		    socket_close($msgsock);
+		} while (true);
+		
+		# Close Socket
+		socket_close($this->socket);
 	}
 	
 	/*	-------------------------------------------------------
@@ -82,6 +126,23 @@ class Platform {
 					$this->context										= $tmp;
 				}
 			}
+		}
+	}
+	
+	private function init_socket() {
+		# Create Socket Object
+		if (($this->socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
+		    throw new Exception("socket_create() failed: reason: " . socket_strerror(socket_last_error()));
+		}
+		
+		# Bind Socket to Address and Port
+		if (socket_bind($this->socket, $this->address, $this->port) === false) {
+		    throw new Exception("socket_bind() failed: reason: " . socket_strerror(socket_last_error($this->socket)));
+		}
+		
+		# Set Socket to Listen for incommning connections
+		if (socket_listen($this->socket, 5) === false) {
+		    throw new Exception("socket_listen() failed: reason: " . socket_strerror(socket_last_error($this->socket)));
 		}
 	}
 	
